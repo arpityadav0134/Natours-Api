78fddcd5838169310fa2cb7eac6ec6f2efabe390
Arpit Yadav <arpityadav0134@gmail.com>
2022-07-08 16:07:23 +0530

    Fixed some more bugs

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -166,7 +166,6 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
   }
   // startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
   /********************************************************************************/
-  console.log(distance, lat, lng, unit);
 
   let filter = { startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } } };
 
